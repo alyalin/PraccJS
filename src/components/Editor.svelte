b744c68f2be2821e1794adbbfd8cffe490278196
Alex Lyalin <occvlt@yandex.ru>
2025-01-26 13:36:39 +0400

    add focus to editor when user creates tab

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import type { ITab } from "../stores/tabs";
     import Monaco from "./Monaco.svelte";
+    import { editor } from "monaco-editor";
 
     interface EditorProps extends Pick<ITab, 'id' | 'content'> {
         onModelChange: (id: string, value: string) => void,
@@ -9,10 +11,21 @@
     let { onModelChange, id, content }: EditorProps =
         $props();
 
+    onMount(() => {
+
+    });
     
     function handleModelChange(content: string) {
         onModelChange(id, content);
     }
+
+    function handleMonacoReady(editor: editor.IStandaloneCodeEditor) {
+        $effect(() => {
+            if (!editor.getValue().length) {
+                editor.focus();
+            }
+        })
+    }
 </script>
 
-<Monaco content={content} onModelChange={handleModelChange} />
+<Monaco content={content} onModelChange={handleModelChange} onMonacoReady={handleMonacoReady} />
